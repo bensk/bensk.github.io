2f19320b4af02bfe866a7f536ebf6c20f9bec7dc
SK <bsk@ms223.org>
2015-01-14 17:39:31 -0500

    Added JavaScript Review Sheet

---
+++ JavaScript Review.js
@@ -0,0 +1,150 @@
+// JavaScript
+// Test
+// Review 
+
+// Big topics. Make sure you can explain what each of these do in code. 
+
+console.log()
+prompt()
+confirm()
+if (true) {} else{};
+if (true) {};
+"Length".length
+2+2=
+confirm("I am ready to go.")
+prompt("What is going on?");
+
+// Data Types
+
+42 // what data type is this? 
+42.5 // what data type is this? 
+"Strings" // what data type is this?  
+5 > 2, true + false // what data type is this? 
+var testReview = true // what data type is this? 
+
+// Examples
+
+if ( 100 > 2 )
+{
+    console.log("You are good at math!");
+}
+
+console.log("Just letting you know: your program got to line 6");
+
+//
+
+if (5 < 2)
+{
+    alert("Let's go down the first road!");
+}
+else
+{
+    console.log(alert("Hope I can do JavaScript"))
+    
+}
+
+
+// Remember, the order and punctuation matter.
+// If you get an error, check carefully, line by line.
+// What does this codoe do? 
+
+if (5 < 2)
+{        
+    console.log("I am right")
+}
+else
+{
+    console.log("I am wrong")
+}
+
+// The computer doesn't worry about extra spaces between words or brackets
+// It just cares about the order of where things are placed
+// and that you have used the right characters (){}[]"";
+
+// For extra help, a program called a 'linter' is checking your code
+// and will put a red 'x' next to the first line that contains errors.
+
+if (10 >= 10) {
+    console.log("You got a true!");
+} else {
+    console.log("You got a false!");
+}
+
+//An example of an if/else statement with modulo in the condition
+
+if( 10%2===0 ) {
+    console.log("The first number is even!");
+} else {
+    console.log("The first number is odd!");
+}
+
+// Remember your logic rules too...
+
+"Modus Ponens" // What does this mean?
+
+"Modus Tollens" // What does this mean? 
+
+// Use console.log( ) to print out the substrings.
+// Here is an example of the 1st to 4th letter of "JavaScript":
+// console.log("JavaScript".substring(0,4));
+
+// a. "Jan" in "January"
+// b. "Melbourne is" in "Melbourne is great" (note the space!)
+// c. "burgers" in "Hamburgers"
+
+console.log("January".substring(0,3));
+console.log("Melbourne is great".substring(0,12));
+console.log("Hamburgers".substring(3,10));
+
+// Declare a variable on line 3 called
+// myCountry and give it a string value.
+var myCountry="USA"
+
+// Use console.log to print out the length of the variable myCountry.
+console.log(myCountry.length);
+
+// Use console.log to print out the first three letters of myCountry.
+console.log(myCountry.substring(0,3));
+
+// Explain what this code does.
+if (3>2) {
+  console.log("I finished my first course!");
+} else {
+    console.log("Oops");
+}
+
+// Write a function that returns the product of two numbers
+function function_name (argument) {
+  // body...
+}
+
+function myFunction(p1, p2) {
+    return p1 * p2;  
+}
+
+  // oooo$$$$$$$$$$$$oooo
+                      // oo$$$$$$$$$$$$$$$$$$$$$$$$o
+                   // oo$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o         o$   $$ o$
+   // o $ oo        o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o       $$ $$ $$o$
+// oo $ $ "$      o$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$o       $$$o$$o$
+// "$$$$$$o$     o$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$o    $$$$$$$$
+  // $$$$$$$    $$$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$$$$$
+  // $$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$$$$$$  """$$$
+   // "$$$""""$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     "$$$
+    // $$$   o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     "$$$o
+   // o$$"   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       $$$o
+   // $$$    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" "$$$$$$ooooo$$$$o
+  // o$$$oooo$$$$$  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   o$$$$$$$$$$$$$$$$$
+  // $$$$$$$$"$$$$   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     $$$$""""""""
+ // """"       $$$$    "$$$$$$$$$$$$$$$$$$$$$$$$$$$$"      o$$$
+            // "$$$o     """$$$$$$$$$$$$$$$$$$"$$"         $$$
+              // $$$o          "$$""$$$$$$""""           o$$$
+               // $$$$o                                o$$$"
+                // "$$$$o      o$$$$$$o"$$$$o        o$$$$
+                  // "$$$$$oo     ""$$$$o$$$$$o   o$$$$""
+                     // ""$$$$$oooo  "$$$o$$$$$$$$$"""
+                        // ""$$$$$$$oo $$$$$$$$$$
+                                // """"$$$$$$$$$$$
+                                    // $$$$$$$$$$$$
+                                     // $$$$$$$$$$"
+                                      // "$$$""  
