ffb76d82aea6e97f7e66a13feed49f3c45c4b340
Mr. S-K <bsk@ms223.org>
2015-10-11 14:52:36 -0400

    updated stuff

---
+++ SE8/WebDesign/<span>getElementById/js/index.js
@@ -0,0 +1,10 @@
+var changeColor = function(newColor){
+	var color=document.getElementById("red");
+	var color1=document.getElementById("blue");
+	var color2=document.getElementById("green");
+	color.style.color=newColor;
+	color1.style.color=newColor;
+	color2.style.color=newColor;
+}
+
+// Break the logic for a quiz!
