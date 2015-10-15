1765d4c46899dda62d549a141cbc35cd7a7cd6d0
SK <bsk@ms223.org>
2015-10-15 15:53:25 -0400

    google logo

---
+++ SE8/WebDesign/GoogleLogo/js/script.js
@@ -0,0 +1,31 @@
+var googleColors=function(color, color1, color2, color3){
+	var gColor = document.getElementById("g");
+	var oColor = document.getElementById("o");
+	var o1Color = document.getElementById("o1");
+	var g1Color = document.getElementById("g1");
+	var lColor = document.getElementById("l");
+	var eColor= document.getElementById("e");
+	gColor.style.color = color;
+	oColor.style.color= color1;
+	o1Color.style.color=color2;
+	g1Color.style.color=color;
+	lColor.style.color=color3;
+	eColor.style.color=color1;
+	
+}
+
+var blackOut=function(color){
+	var gColor = document.getElementById("g");
+	var oColor = document.getElementById("o");
+	var o1Color = document.getElementById("o1");
+	var g1Color = document.getElementById("g1");
+	var lColor = document.getElementById("l");
+	var eColor= document.getElementById("e");
+	gColor.style.color = color;
+	oColor.style.color= color;
+	o1Color.style.color=color;
+	g1Color.style.color=color;
+	lColor.style.color=color;
+	eColor.style.color=color;
+	
+}
