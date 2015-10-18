f44cf701d29d479aaadca833454c264ddaddcae8
Mr. S-K <bsk@ms223.org>
2015-10-18 15:20:16 -0400

    updated emoji switch

---
+++ SE8/WebDesign/emojiswitch/js/index.js
@@ -0,0 +1,8 @@
+var changeEmoji = function() {
+		var emoji = document.getElementById("emoji");
+		if (emoji.innerHTML === "👍") {
+				emoji.innerHTML = "👎";
+		} else {
+				emoji.innerHTML = "👍";
+		}
+}
