625720be21e5ccf270560ede054505b8d271da81
Mr. S-K <bsk@ms223.org>
2015-11-01 17:54:39 -0500

    welp

--- SE8/WebDesign/GoogleLogo/js/script.js
+++ SE8/WebDesign/GoogleLogo/js/script.js
@@ -11,12 +11,12 @@ var googleColors=function(color, color1, color2, color3){
 	g1Color.style.color=color;
 	lColor.style.color=color3;
 	eColor.style.color=color1;
-	
+
 }
 
 var blackOut=function(color){
-	var gColor = document.getElementById("g");
-	var oColor = document.getElementById("o");
+	var gColor = document.getElementById("b");
+	var oColor = document.getElementById("e");
 	var o1Color = document.getElementById("o1");
 	var g1Color = document.getElementById("g1");
 	var lColor = document.getElementById("l");
@@ -27,5 +27,5 @@ var blackOut=function(color){
 	g1Color.style.color=color;
 	lColor.style.color=color;
 	eColor.style.color=color;
-	
+
 }
