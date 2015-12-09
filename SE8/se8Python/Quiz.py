492e13726ee2ace3d307bc51c19c5ba9da391f68
Mr. S-K <bsk@ms223.org>
2015-12-08 22:03:04 -0500

    python

---
+++ SE8/se8Python/Quiz.py
@@ -0,0 +1,11 @@
+harry_potter = raw_input("What's Harry Potter's middle name?")
+if harry_potter.lower() == "james":
+    print "You're ok"
+else:
+    print "Expeliarmus!"
+
+grace_hopper = raw_input("Grace Hopper coined what Computer Science term? A) Byte, B) Bug, C) Iterate, D) Function")
+if grace_hopper.lower() == "b":
+    print "You remembered!"
+else:
+    print "No! You make me sad..."
