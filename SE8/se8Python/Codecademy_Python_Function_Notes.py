8e429cce5fbfdb6af83e8d20333966eb5551d461
bsk <bsk@ms223.org>
2015-12-13 08:43:06 -0500

    updated notes

--- SE8/se8Python/Codecademy_Python_Function_Notes.py
+++ SE8/se8Python/Codecademy_Python_Function_Notes.py
@@ -29,4 +29,87 @@ print math.sqrt(25)
 # Exercise 11
 import math            # Imports the math module
 everything = dir(math) # Sets everything to a list of things from math
-print everything       # Prints 'em all! 
+print everything       # Prints 'em all!
+
+# Exercise 18
+
+from math import sqrt # one way to import
+print sqrt(13689)
+
+import math # another way to import
+print math.sqrt(9)
+
+# Exericse 19 Review Built-in Functions
+def is_numeric(num):
+    return type(num) == int or type(num) == float:
+
+max(2, 3, 4) # 4
+min(2, 3, 4) # 2
+
+abs(2) # 2
+abs(-2) # 2
+
+def distance_from_zero(num):
+    if type(num) == int or type(num) == float:
+        return abs(num)
+    else:
+        return "Nope"
+
+# More Functions
+"""This is my way. Codecademy does it different below"""
+def hotel_cost(nights):
+    return 140 * nights
+def plane_ride_cost(city):
+    if city == "charlotte".lower():
+        return 183
+    elif city == "tampa".lower():
+        return 220
+    elif city == "pittsburgh".lower():
+        return 222
+    elif city == "los angeles".lower():
+        return 475
+
+print plane_ride_cost(raw_input("Which city?"))
+"""Codecademy way"""
+def hotel_cost(nights):
+    return 140 * nights
+def plane_ride_cost(city):
+    if city == "Charlotte":
+        return 183
+    elif city == "Tampa":
+        return 220
+    elif city == "Pittsburgh":
+        return 222
+    elif city == "Los Angeles":
+        return 475
+def rental_car_cost(days):
+    cost = days * 40
+    if days >= 7:
+        cost = cost - 50
+    elif days >=3:
+        cost = cost -20
+    return cost
+
+# Exercise 7 Final trip_cost function
+def hotel_cost(nights):
+    return 140 * nights
+def plane_ride_cost(city):
+    if city == "Charlotte":
+        return 183
+    elif city == "Tampa":
+        return 220
+    elif city == "Pittsburgh":
+        return 222
+    elif city == "Los Angeles":
+        return 475
+def rental_car_cost(days):
+    cost = days * 40
+    if days >= 7:
+        cost = cost - 50
+    elif days >=3:
+        cost = cost -20
+    return cost
+def trip_cost(city, days, spending_money):
+    return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
+
+print trip_cost("Los Angeles", 5, 600)
