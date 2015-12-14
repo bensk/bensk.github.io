4641f30bc7dd8bca70139e2bc595f80cb8192f1c
SK <bsk@ms223.org>
2015-12-14 14:02:12 -0500

    Emoji!

--- SE8/se8Python/Quiz.py
+++ SE8/se8Python/Quiz.py
@@ -2,17 +2,16 @@ score = 0
 
 harry_potter = raw_input("What's Harry Potter's middle name?")
 if harry_potter.lower() == "james":
-    print "You're ok"
-    score = 1
+    print u"⚡You're ok⚡" # To get emoji to print correctly, put a u for 'unicode' before a string.
+    score = score + 1
 else:
-    print "Expeliarmus!"
-
+    print u"💥Expeliarmus!💥"
 
 grace_hopper = raw_input("Grace Hopper coined what Computer Science term? A) Byte, B) Function, C) Iterate, D) Debug")
 if grace_hopper.lower() == "d" or grace_hopper.lower()=="debug":
-    print "You remembered!"
+    print u"🐞You remembered!🐞"
     score = score + 1
 else:
-    print "No! You make me sad..."
+    print u"😭No! You make me sad...😭"
 
-print "Your score is " + str(score) + " / 5"
+print "Your score is " + str(score) + " / 2"
