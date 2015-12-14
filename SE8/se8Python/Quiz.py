e5f1e49b6de6e59bc42096b2c54dad899b76dbdc
SK <bsk@ms223.org>
2015-12-14 07:17:13 -0500

    fixed terms

--- SE8/se8Python/Quiz.py
+++ SE8/se8Python/Quiz.py
@@ -1,11 +1,18 @@
+score = 0
+
 harry_potter = raw_input("What's Harry Potter's middle name?")
 if harry_potter.lower() == "james":
     print "You're ok"
+    score = 1
 else:
     print "Expeliarmus!"
 
-grace_hopper = raw_input("Grace Hopper coined what Computer Science term? A) Byte, B) Bug, C) Iterate, D) Function")
-if grace_hopper.lower() == "b":
+
+grace_hopper = raw_input("Grace Hopper coined what Computer Science term? A) Byte, B) Function, C) Iterate, D) Debug")
+if grace_hopper.lower() == "d" or grace_hopper.lower()=="debug":
     print "You remembered!"
+    score = score + 1
 else:
     print "No! You make me sad..."
+
+print "Your score is " + str(score) + " / 5"
