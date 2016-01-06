dd474f56f3a1229247c3b3edd432d119fc1a8519
SK <bsk@ms223.org>
2016-01-06 14:18:54 -0500

    for 1/7 lesson

---
+++ SE8/se8Python/Jan6PythonScratch.py
@@ -0,0 +1,18 @@
+do_now = ["words", "stuff", "wrong", "right", "question", "got", "you"]
+
+# Exercise 4
+
+suitcase = []
+suitcase.append("sunglasses")
+
+# Your code here!
+suitcase.append("shirts")
+suitcase.append("shoes")
+suitcase.append("pants")
+
+
+
+list_length = len(suitcase) # Set this to the length of suitcase
+
+print "There are %d items in the suitcase." % (list_length)
+print suitcase
