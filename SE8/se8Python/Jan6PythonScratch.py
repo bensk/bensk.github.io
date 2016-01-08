bd41b4255167f104365e390226d4032dc96c3203
SK <bsk@ms223.org>
2016-01-08 14:45:02 -0500

    added iPython notebook

--- SE8/se8Python/Jan6PythonScratch.py
+++ SE8/se8Python/Jan6PythonScratch.py
@@ -1,7 +1,13 @@
+<<<<<<< Updated upstream
 do_now = ["words", "stuff", "wrong", "right", "question", "got", "you"]
 
 
 
+=======
+do_now = ["words", "stuff", "wrong", "this", "right", "question", "got", "you"]
+#              0       1       2       3       4       5             6     7
+print do_now[7] + do_now[6] + do_now[3] + do_now[2]
+>>>>>>> Stashed changes
 # Exercise 4
 
 suitcase = []
@@ -23,3 +29,6 @@ list_length = len(suitcase) # Set this to the length of suitcase
 
 print "There are %d items in the suitcase." % (list_length)
 print suitcase
+
+print len(classes)
+# Prints the length, which is...
--- SE8/se8Python/MadLibsListsAndLoops.py
+++ SE8/se8Python/MadLibsListsAndLoops.py
@@ -2,10 +2,9 @@ import random
 
 def madLibs():
   names = raw_input("Give a bunch of names").split()
-  adjectives = raw_input("Give a bunch of").split()
-  name_index = len(names) - 1
+  adjectives = raw_input("Give a bunch of adjectives").split()
+  
   for name in names:
-    print names[name_index] + " is " + random.choice(adjectives)
-    name_index = name_index - 1
+    print name + " is " + random.choice(adjectives)
 
 madLibs()
