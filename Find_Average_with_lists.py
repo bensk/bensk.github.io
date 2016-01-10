ec3a2938b5dbe7fbd7f4f8c2ae101ab288128cc2
bsk <bensamuelskalow@gmail.com>
2016-01-10 16:22:03 -0500

    python or something

---
+++ Find_Average_with_lists.py
@@ -0,0 +1,12 @@
+grades = raw_input("What are your grades?").split() # Start out just like our old GPA calculator.
+float_grades=[] # Make an empty list. We'll need this once we convert all of our grades to floats.
+
+for inputs in grades: # This for loop will go through every grade...
+  num_grade = float(inputs) # float() the value of each grade...
+  float_grades.append(num_grade) # and append that float value to our empty list
+
+# I've made a new variable. You don't NEED to do this, but it makes the last line look cleaner
+average = sum(float_grades)/len(float_grades)
+
+# Now that we're ready to print, we can just str() our average variable.
+print "Your average is a " + str(average)
