97e7f917fbf0347042b9adcae6dcaece03389341
SK <bsk@ms223.org>
2016-01-13 17:34:12 -0500

    updated notes

--- SE8/se8Python/Codecademy_Python_Lists_Notes.py
+++ SE8/se8Python/Codecademy_Python_Lists_Notes.py
@@ -143,3 +143,97 @@ zoo_animals['Rockhopper Penguin'] = 'Panda'
 
 
 print zoo_animals
+
+"""SuperMarket"""
+#Exercise 1
+names = ["Adam","Alex","Mariah","Martine","Columbus"]
+
+for n in names:
+    print n
+
+# Exercise 2
+webster = {
+	"Aardvark" : "A star of a popular children's cartoon show.",
+    "Baa" : "The sound a goat makes.",
+    "Carpet": "Goes on the floor.",
+    "Dab": "A small amount."
+}
+
+# Add your code below!
+for k in webster:
+    print webster[k]
+
+# Exercise 3
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+for numbers in a:
+    if numbers%2==0:
+        print numbers
+
+# Exercise 4
+
+# Write your function below!
+def fizz_count(x):
+    count = 0
+    for item in x:
+        if item == "fizz":
+            count = count+1
+    return count
+
+# Exercise 5
+for letter in "Codecademy":
+    print letter
+
+# Empty lines to make the output pretty
+print
+print
+
+word = "Programming is fun!"
+
+for letter in word:
+    # Only print out the letter i
+    if letter == "i":
+        print letter
+
+# Exercise 6
+prices = {
+    "banana": 4,
+	"apple": 2,
+	"orange": 1.5,
+	"pear": 3
+}
+
+# Exercise 7
+prices = {
+    "banana": 4, # Don't forget commas after values in the key
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15,
+}
+
+# Exercise 8
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15,
+}
+
+for key in prices:
+    print key
+    print "price: %d" %prices[key]
+    print "stock: %d" %stock[key]
