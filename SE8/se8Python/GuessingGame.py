76b4e11f53b3dbdfb779ad792d316af501e4b7cc
Mr. S-K <bsk@ms223.org>
2016-01-18 18:10:42 -0500

    updated game with score

--- SE8/se8Python/GuessingGame.py
+++ SE8/se8Python/GuessingGame.py
@@ -1,4 +1,4 @@
-user_name = raw_input("What's your name?").upper()
+user_name = raw_input("What's your name?").capitalize()
 print "Hello there " + user_name
 
 upper_bound = int(raw_input("What's the largest number you'd like to choose?"))
@@ -8,14 +8,16 @@ from random import randint
 answer = randint(0, upper_bound)
 
 guess = int(raw_input("What's your guess?"))
-
+score = 0
 while guess != answer:
     # Got it to give me feedback!
     if guess>answer:
         print "Your guess is too high!"
+        score = score + 1
     else:
         print "Your guess is too low!"
+        score = score + 1
     guess = int(raw_input("What's your guess?"))
-
-print "Great job, " + user_name + ", the number was " + str(answer)
+score = score+1
+print "Great job, " + user_name + ", the number was " + str(answer) + ". It only too you " + str(score) + " tries."
 # print randint(0,upper_bound) so, that works
