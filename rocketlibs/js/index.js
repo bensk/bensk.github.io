4d0fde1d24e9f24a0c5066b6770fa7dbdc986e5d
bsk <bensamuelskalow@gmail.com>
2016-02-04 12:48:35 -0500

    updated story

--- rocketlibs/js/index.js
+++ rocketlibs/js/index.js
@@ -3,6 +3,7 @@ function madlibs() {
   var name = document.getElementById("Name").value;
   var adjective = document.getElementById("Adjective").value;
   var noun = document.getElementById("Noun").value;
+  var noun1 = document.getElementById("Noun1").value;
   var verb = document.getElementById("Verb").value;
-  story.innerHTML = "A" + " " + "important" +" " + noun +" " +" " + "variable" + " " +"named" + " " + name + "'s" + " " + adjective + " " + "rocket boys" + " " + verb + " " + "to" + " " + " the " + " " + "moon" + " ! " + " ! ";
+  story.innerHTML = "When building rockets, don't rely on " + noun + ". First, you need to come up with a " + adjective + "hypothesis for your project. If your experiment doesn't work (or is destroyed on impact), you will need to adjust your calculation. Another considerable factor in building rockets is " + noun1 + ". You need to generate sufficient thrust in order to " + "verb" + " correctly.";
 }
