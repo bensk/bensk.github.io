9337b687c17cce14f0e455b3c50c6822cc9d7c75
Mr. S-K <bsk@ms223.org>
2016-03-02 22:45:36 -0500

    LIGHTNING

---
+++ js/emojifollow.js
@@ -0,0 +1,7 @@
+ ! function() {
+ 	document.body.style.cursor = document.body.parentNode.style.cursor = "none"
+ 	var e = document.createElement("div")
+ 	e.innerHTML = "⚡️", e.style.fontSize = "30px", e.style.position = "absolute", e.style.zIndex = 9999, e.style.pointerEvents = "none", document.body.appendChild(e), window.addEventListener("mousemove", function(t) {
+ 		e.style.left = t.pageX + "px", e.style.top = t.pageY + "px"
+ 	})
+ }()
