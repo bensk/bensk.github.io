ac5453f21f14ced111ac3a685d9c07ce158a6bd2
Mr. S-K <bsk@ms223.org>
2016-03-02 22:52:20 -0500

    pick random emoji!

--- js/emojifollow.js
+++ js/emojifollow.js
@@ -1,7 +1,11 @@
+// Write something to select a random emoji
+var myArray = ['⚡️', '💩', '💡', '🍑', '👍', '🌮', '👾', '🖥'];
+var rand = myArray[Math.floor(Math.random() * myArray.length)];
+
  ! function() {
  	document.body.style.cursor = document.body.parentNode.style.cursor = "none"
  	var e = document.createElement("div")
- 	e.innerHTML = "⚡️", e.style.fontSize = "30px", e.style.position = "absolute", e.style.zIndex = 9999, e.style.pointerEvents = "none", document.body.appendChild(e), window.addEventListener("mousemove", function(t) {
+ 	e.innerHTML = rand, e.style.fontSize = "30px", e.style.position = "absolute", e.style.zIndex = 9999, e.style.pointerEvents = "none", document.body.appendChild(e), window.addEventListener("mousemove", function(t) {
  		e.style.left = t.pageX + "px", e.style.top = t.pageY + "px"
  	})
  }()
